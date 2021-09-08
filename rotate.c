ad90328331ef10829c14cd6e74df8d0dcdda72a3
isha2312 <80533092+isha2312@users.noreply.github.com>
2021-09-08 17:31:48 +0530

    Update rotate.c

--- rotate.c
+++ rotate.c
@@ -2,65 +2,41 @@
 struct ListNode* rotateRight(struct ListNode* head, int k)
 {
  
-    // If list is empty
-    if (!head)
-        return head;
-
+   if(head==NULL)
+       return NULL;
     
-    struct ListNode* temp = head;
+    //finding length of the list
+    struct ListNode* temp=head;
+    int size=0;
+     while(temp!=NULL)
+     {
+         size++;
+         temp=temp->next;
+     }
     
-    int c = 1;  // c for storing length of the list
+    k=k % size;
     
-    while (temp->next != NULL) 
+    struct ListNode* p1=head;
+    struct ListNode* p2=head;
+    
+    //moving p2 by k nodes
+    for(int i=0;i<k;i++)
     {
-        temp = temp->next;
-        c++;
+        p2=p2->next;
     }
-  // temp is pointing to the last node now
-
     
- 
-    // If size of list<k then taking the modulus
-    if (k > c)
-        k = k % c;
- 
-    // Subtracting k from length to convert it to left rotations
-    k = c - k;
- 
-    // If no rotation needed then returning head
-    if (k == 0 || k == c)
-        return head;
-
-    
-    struct ListNode* current = head;
-    int count = 1;
-    while (count < k && current != NULL) 
+    while(p2->next!=NULL)
     {
-        current = current->next;
-        count++;
+        p1=p1->next;
+        p2=p2->next;
     }
-    //current points to kth node now or to NULL
+     
+    p2->next=head;
     
- 
-    // If current = NULL then it means k = count of nodes
-    if (current == NULL)
-        return head; //no change in this case
+    struct ListNode* newhead;
+     newhead=p1->next;
+     p1->next=NULL;
     
- 
-    // If current points to the k node
-    struct ListNode* knode = current;
- 
-    // Change next of last node to previous head
-    temp->next = head;
- 
-    // Change head to (k+1)th node
-    head = knode->next;
- 
-    // Change next of kth node to NULL
-    knode->next = NULL;
- 
-    // Return the updated head 
-    return head;
-  
+    return newhead;
     
-}
+    }
